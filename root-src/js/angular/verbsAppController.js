6b33e3c921cf2ad7f671b961e3ba7ceb30528bda
Craig Morey <craig.morey@stendahls.se>
2015-04-15 12:48:13 +0200

    fix prefixes in revealing translations, fix iOS not receiving scroll events

--- root-src/js/angular/verbsAppController.js
+++ root-src/js/angular/verbsAppController.js
@@ -3,17 +3,49 @@ verbsApp.controller('VerbsListController', ['$scope', 'verbsFactory', function($
     $scope.verbs = [];
     $scope.verbsCount = 0;
 
+    var prefix = (function () {
+        var styles = window.getComputedStyle(document.documentElement, ''),
+            pre = (Array.prototype.slice
+                .call(styles)
+                .join('') 
+                .match(/-(moz|webkit|ms)-/)
+            )[1] || '',
+            dom = ('WebKit|Moz|MS|O').match(new RegExp('(' + pre + ')', 'i'))[1];
+
+
+        var jsPrefix = pre[0].toUpperCase() + pre.substr(1);
+        var transformPrefixed = 'tranform';
+        var transitionPrefixed = 'transition';
+        if (dom.length) {
+            transformPrefixed = pre + 'Transform';
+            transitionPrefixed = pre + 'Transition';
+        }
+        return {
+            dom: dom,
+            lowercase: pre,
+            css: '-' + pre + '-',
+            js: jsPrefix,
+            transform: transformPrefixed,
+            transition: transitionPrefixed
+        };
+    })();
+
     $scope.panRight = function(event) {
         var deltaX = event.deltaX;
+        var deltaY = event.deltaY;
+        if (Math.abs(deltaY)/3 > Math.abs(deltaX) || deltaY > 20 ) {
+            event.element.context.style[prefix.transform] = 'translate(0,0)';
+            return;
+        }
         if (deltaX > 0) {
-            event.element.context.style.transform = 'translate(' + deltaX + 'px,0)';
+            event.element.context.style[prefix.transform] = 'translate(' + deltaX + 'px,0)';
         }
     }
     $scope.panEnd = function(event) {
-        event.element.context.style.transition = 'transform 200ms';
-        event.element.context.style.transform = 'translate(0,0)';
+        event.element.context.style[prefix.transition] = prefix.css + 'transform 200ms';
+        event.element.context.style[prefix.transform] = 'translate(0,0)';
         setTimeout(function() {
-            event.element.context.style.transition = 'none';
+            event.element.context.style[prefix.transition] = 'none';
         },200)
     }
 
