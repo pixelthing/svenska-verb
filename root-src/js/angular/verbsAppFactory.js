74bd1ae12bb31d2c9363bb26f5d74eb9076dd249
Craig Morey <craigmorey@gmail.com>
2015-05-01 11:52:46 +0200

    more verbs, start processing conjugation

--- root-src/js/angular/verbsAppFactory.js
+++ root-src/js/angular/verbsAppFactory.js
@@ -1,12 +1,20 @@
 verbsApp.factory('verbsFactory', ['$http', function verbsFactory($http) {
 
+    var conjugate = function(array) {
+        return array;
+    }
+
     var getData = function() {
 
         //return the promise directly.
         return $http.get('svenska.json')
             .then(function(result) {
                 //resolve the promise as the data
-                return result.data.data.verbs;
+                var raw = result.data.data.verbs;
+                // process the presens/preteritum/perfekt
+                var processed = conjugate(raw);
+                // return the processed data
+                return processed;
             }, function() {
                 alert('error loading data - sorry!');
             });
