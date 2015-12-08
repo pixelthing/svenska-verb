ba46a4202959188e3dac691e301cdd2fb27a41de
Craig Morey <craigmorey@gmail.com>
2015-12-08 09:29:22 +0100

    JS: split out page lock (+ iOS text resize adjust)

---
+++ root-src/js/pageLock.js
@@ -0,0 +1,39 @@
+var pageLockController = function () {
+
+    var modalOffset = null;
+
+    // DETAIL
+
+    var lockPage = function() {
+        modalOffset = document.body.scrollTop || document.documentElement.scrollTop;
+        document.querySelector('html').classList.add('modal');
+        document.querySelector('body').style.top = '-' + modalOffset + 'px';
+        document.querySelector('html').addEventListener('click', pageLockController.backgroundClick);
+    }
+
+    var unLockPage = function() {
+        document.querySelector('html').classList.remove('modal');
+        document.body.scrollTop = modalOffset;
+        document.querySelector('body').style.top = 'auto';
+        modalOffset = null;
+        document.querySelector('html').addEventListener('click', pageLockController.backgroundClick);
+    }
+
+    // wehn pageLock is active, a background click (that isn't within the pageLock) will unlock the page and trigger the closure of any modals/menus
+    var backgroundClick = function(ev) {
+        console.log('1111')
+        //var $this = $(ev.target);
+        //if ($this.is('[data-js-pageLock-el]') || $this.closest('[data-js-pageLock-el]').length > 0) {
+        //    return;
+        //}
+        //pageLockController.unLockPage();
+    }
+
+    // CLOSURE
+
+    return {
+        lockPage: lockPage,
+        unLockPage: unLockPage
+    }
+
+}();
