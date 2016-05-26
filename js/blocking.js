08b80c556f620d5274c2fae0efe5e08814138c32
Craig Morey <craigmorey@gmail.com>
2016-05-26 23:23:52 +0200

    Updates

--- js/blocking.js
+++ js/blocking.js
@@ -2,17 +2,14 @@ var isSafari = /constructor/i.test(window.HTMLElement);
 if (isSafari) {
     document.querySelector('html').classList.add('safari');
 }
-
 var isAndroid = /android/i.test(navigator.userAgent.toLowerCase());
 if (isAndroid) {
     document.querySelector('html').classList.add('android');
 }
-
 var isWM = /iemobile/i.test(navigator.userAgent.toLowerCase());
 if (isWM) {
     document.querySelector('html').classList.add('winMob');
 }
-
 if (
     ("standalone" in window.navigator) &&
     !window.navigator.standalone
---
+++ sw.js
@@ -0,0 +1,21 @@
+(global => {
+  'use strict';
+
+  // Load the sw-toolbox library.
+  importScripts('/js/sw-toolbox.js');
+  
+  global.toolbox.options.debug = true;
+  
+  // Ensure that our service worker takes control of the page as soon as possible.
+  global.addEventListener('install', event => event.waitUntil(global.skipWaiting()));
+  global.addEventListener('activate', event => event.waitUntil(global.clients.claim()));
+  
+  toolbox.router.get('/(.*)', toolbox.fastest, {
+    cache: {
+      name: 'svenskaVerb1',
+      maxEntries: 10
+    }
+  });
+  toolbox.precache(['/index.html', '/css/main.css', '/js/blocking.js', '/js/nonblocking.js', '/svenska.json']);
+
+})(self);
