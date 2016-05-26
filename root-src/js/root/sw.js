d4f6f71015f64d4de66fceba3e22210f88a477ec
Craig Morey <craigmorey@gmail.com>
2016-05-26 23:20:48 +0200

    Service worker v1!

--- root-src/js/root/sw.js
+++ root-src/js/root/sw.js
@@ -1,13 +1,21 @@
-toolbox.router.get('/index.html', toolbox.fastest);
-toolbox.precache(['/index.html', '/css/main.css', '/js/blocking.js', '/js/nonblocking.js', '/svenska.json']);
-
 (global => {
   'use strict';
 
   // Load the sw-toolbox library.
   importScripts('/js/sw-toolbox.js');
   
+  global.toolbox.options.debug = true;
+  
   // Ensure that our service worker takes control of the page as soon as possible.
   global.addEventListener('install', event => event.waitUntil(global.skipWaiting()));
   global.addEventListener('activate', event => event.waitUntil(global.clients.claim()));
+  
+  toolbox.router.get('/(.*)', toolbox.fastest, {
+    cache: {
+      name: 'svenskaVerb1',
+      maxEntries: 10
+    }
+  });
+  toolbox.precache(['/index.html', '/css/main.css', '/js/blocking.js', '/js/nonblocking.js', '/svenska.json']);
+
 })(self);
